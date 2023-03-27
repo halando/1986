b29dc7ce85b208739513a7a08ceb4482190f0112
Andó Attila <andoattila698@gmail.com>
2023-03-27 15:48:52 +0200

    1986

---
+++ src/views/MainView.java
@@ -0,0 +1,15 @@
+package views;
+
+import javafx.scene.layout.VBox;
+
+public class MainView  extends VBox{
+    TablePanel tablePanel;
+    public MainView() {
+        this.tablePanel = new TablePanel();
+        this.getChildren().add(this.tablePanel);
+    }
+
+
+    
+    
+}
