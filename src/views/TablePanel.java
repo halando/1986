4471e9e10515ebdb4cbe4cf62c42b8c819f2807b
Andó Attila <andoattila698@gmail.com>
2023-03-27 16:37:11 +0200

    Tata 1986

--- src/views/TablePanel.java
+++ src/views/TablePanel.java
@@ -1,11 +1,36 @@
 package views;
 
+import javafx.beans.property.Property;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.Employee;
 
-public class TablePanel extends TableView<Employee>{
+public class TablePanel extends TableView<Employee> {
+    TableColumn<Employee, String> name_col;
+    TableColumn<Employee, String> city_col;
+    TableColumn<Employee, String> salary_col;
 
     public TablePanel() {
+        this.createColumns();
+        this.addColumns();
+
+    }
+
+    private void createColumns() {
+        this.name_col = new TableColumn<>("Név");
+        this.name_col.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.city_col = new TableColumn<>("Település");
+        this.city_col.setCellValueFactory(new PropertyValueFactory<>("city"));
+        this.salary_col = new TableColumn<>("Fizetés");
+        this.salary_col.setCellValueFactory(new PropertyValueFactory<>("salary"));
+    }
+
+    private void addColumns() {
+        this.getColumns().add(this.name_col);
+        this.getColumns().add( this.city_col);
+        this.getColumns().add(this.salary_col);
+        
     }
     
 }
